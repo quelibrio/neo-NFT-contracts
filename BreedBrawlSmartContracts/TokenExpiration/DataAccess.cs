5751fef4b2ba417f4bd847a0b2c4b4afdc38316e
Martin Ivanov <quelibrio@gmail.com>
2018-10-30 23:29:26 +0200

    Token expiration p

---
+++ BreedBrawlSmartContracts/TokenExpiration/DataAccess.cs
@@ -0,0 +1,59 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransfairExpiration
+{
+    public class DataAccess : SmartContract
+    {
+        public static TokenInfo GetToken(byte[] id)
+        {
+            byte[] key = Keys.Token(id);
+            byte[] bytes = Storage.Get(Storage.CurrentContext, id);
+            if (bytes.Length == 0)
+            {
+                return null;
+            }
+            return (TokenInfo)(object)(object[])Neo.SmartContract.Framework.Helper.Deserialize(bytes);
+        }
+
+        /// <summary>
+        /// Get the index of the first gladiator information owned by an address.
+        /// </summary>
+        public static byte[] GetOwnersTokenIdByIndexAsBytes(byte[] owner, BigInteger index)
+        {
+            byte[] key = Keys.TokenOfOwner(owner, index);
+            return Storage.Get(Storage.CurrentContext, key);
+        }
+
+        public static byte[] GetTotalSupplyAsBytes() =>
+            Storage.Get(Storage.CurrentContext, Keys.KeyTotalSupply);
+
+        public static byte[] GetApprovedAddressAsBytes(byte[] tokenId)
+        {
+            byte[] key = Keys.Approval(tokenId);
+            return Storage.Get(Storage.CurrentContext, key);
+        }
+
+        public static void SetTotalSupply(byte[] totalSupply) =>
+            Storage.Put(Storage.CurrentContext, Keys.KeyTotalSupply, totalSupply);
+
+        public static void SetToken(byte[] id, TokenInfo token)
+        {
+            byte[] key = Keys.Token(id);
+            byte[] bytes = Neo.SmartContract.Framework.Helper.Serialize(token);
+            Storage.Put(Storage.CurrentContext, key, bytes);
+        }
+
+        public static void RemoveApproval(byte[] tokenId)
+        {
+            byte[] key = Keys.Approval(tokenId);
+            Storage.Delete(Storage.CurrentContext, key);
+        }
+    }
+}
---
+++ BreedBrawlSmartContracts/TokenExpiration/Events.cs
@@ -0,0 +1,23 @@
+﻿using Neo.SmartContract.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransfairExpiration
+{
+    public class Events : SmartContract
+    {
+        public delegate void deleTransfer(byte[] from, byte[] to, BigInteger value);
+        [DisplayName("transfer")]
+        public static event deleTransfer Transferred;
+        public static void RaiseTransfer(byte[] from, byte[] to, BigInteger value) => Transferred(from, to, value);
+        public delegate void deleApproved(byte[] owner, byte[] approved, BigInteger tokenId);
+        [DisplayName("approve")]
+        public static event deleApproved Approved;
+        public static void RaiseApproved(byte[] owner, byte[] approved, BigInteger tokenId) => Approved(owner, approved, tokenId);
+    }
+}
